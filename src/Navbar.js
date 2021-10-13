ef160fe43dd03106c976466cbd33dd35619bc343
mayanhavoc <mayanhavoc@gmail.com>
2021-10-12 17:01:31 -0700

    dropdown menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
-import './Navbar.css'
+import Nav from 'react-bootstrap/Nav';
+import NavItem from 'react-bootstrap/NavItem';
+import Dropdown from 'react-bootstrap/Dropdown';
+import NavLink from 'react-bootstrap/NavLink';
+import './Navbar.css';
 
 
 class Navbar extends Component {
     render() {
         return (
-            <>
-                <nav class="navbar navbar-light navbar-1 white">
-                    <a class="navbar-brand" href="#"><img src="./images/logo.png" width="50px" alt="micochango logo" /></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
-                    aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span                    class="navbar-toggler-icon"></span></button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent15">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>  
-            </>
+            <div className="mx-4">
+                <img src="./images/logo.png" width="50px" alt="logo" />
+                <Dropdown className="Nav mx-2" as={NavItem}>
+                    <Dropdown.Toggle as={NavLink}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>Home</Dropdown.Item>
+                        <Dropdown.Item>About</Dropdown.Item>
+                        <Dropdown.Item>Work</Dropdown.Item>
+                        <Dropdown.Item>Contact</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </div>
         )
     }
 }
