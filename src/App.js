d9dea35a036948e7186cc77486ed2607ef43d1d1
macadmin <macadmin@MacBook-Pro.attlocal.net>
2022-09-03 09:23:40 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component, } from 'react';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import Projects from './Components/Projects';
-import Projectss from './Components/Projectss';
-// import About from './Components/About';
-// import Contact from './Components/Contact';
-// import Footer from './Components/Footer';
 
 class App extends Component {
   render(){
     return (
       <main> 
-        <Navbar />
-        <Home />
-        {/* <Projects /> */}
-        <Projectss />
-        {/* <About />
-        <Contact /> */}
-        {/* <Footer />  */}
+       
     </main>
     );
   }
--- src/Components/Project.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Project({children}) {
-  return (
-    
-  )
-}
-
-export default Project
